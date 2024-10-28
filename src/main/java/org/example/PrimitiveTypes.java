ab6b4885adbf6ed4084d27c3608b93a3e915d40d
JAIMECASTRO-code <castrojaime1901@gmail.com>
2024-10-28 21:07:02 +0100

    PrimitiveTypes

---
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        boolean isActive = true;
+        System.out.println(isActive);
+        byte Primer= 0;
+        System.out.println(Primer);
+        Short Corto =66;
+        System.out.println(Corto);
+        int variable = 455;
+        System.out.println(variable);
+        long largo = 3243324;
+        System.out.println(largo);
+        double doble = 34.56;;
+        System.out.println(doble);
+        float flotante = 42.34f;;
+        System.out.println(flotante);
+        char letras = 'S';
+        System.out.println(letras);
+        }
+    }
