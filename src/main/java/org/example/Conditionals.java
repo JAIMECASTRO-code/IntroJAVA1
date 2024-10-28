ef500b5b6bde557b9783d640a6fe92275192baf3
JAIMECASTRO-code <castrojaime1901@gmail.com>
2024-10-28 22:50:03 +0100

    conditionals+boolean

---
+++ src/main/java/org/example/Conditionals.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        boolean isActive = true;
+
+        if (isActive) {
+
+            System.out.println("The user is active");
+        }
+        else {
+            System.out.println("The user is not active");
+        }
+    }
+}
