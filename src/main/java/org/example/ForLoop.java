6fefb1e86b1d28ae6dbb59326e232cbbf138a080
JAIMECASTRO-code <castrojaime1901@gmail.com>
2024-10-28 23:28:24 +0100

    ForLoop.imprimir por consola numero del 5 al 10

---
+++ src/main/java/org/example/ForLoop.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        //imprimir por consola los numeros del 5 al 10
+
+        for(byte numero = 5; numero <= 10; numero++){
+            System.out.println(numero);
+        }
+    }
+}
